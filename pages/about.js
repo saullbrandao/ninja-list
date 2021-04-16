bad69c12b4ed0b6158872380023dac3b07c77723
Saull Brandão <saull@outlook.com>
2021-04-16 11:35:46 -0300

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At sunt maxime earum quae in, quaerat mollitia eligendi rem! Voluptas quasi aut pariatur voluptatibus ex inventore officiis labore quam? Maiores, id.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At sunt maxime earum quae in, quaerat mollitia eligendi rem! Voluptas quasi aut pariatur voluptatibus ex inventore officiis labore quam? Maiores, id.</p>
+      </div>
+    </>
+  );
+}
+
+export default About;
